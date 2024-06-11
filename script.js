f28a21c97ab23d942c257e792deeba249aaec959
PROF ROMULO <rjlpena@gmail.com>
2024-06-11 13:39:05 +0000

    ok

---
+++ script.js
@@ -0,0 +1,27 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "pergunta1",
+        alternativas: [
+            "alternativa1",
+            "alternativa2"
+        ]
+    },
+    {
+        enunciado: "pergunta2",
+        alternativas: [
+            "alternativa1",
+            "alternativa2"
+        ]
+    },
+];
+
+
+
+
+
