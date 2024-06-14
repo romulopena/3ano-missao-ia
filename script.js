da151247765cb2455187c360df34b8d6fd1b7098
PROF ROMULO <rjlpena@gmail.com>
2024-06-14 07:49:56 -0300

    style

--- script.js
+++ script.js
@@ -24,10 +24,16 @@ const perguntas = [
 let atual = 0;
 let perguntaAtual;
 
-function mostraPergunta(){
+//function mostraPergunta(){
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
-}
+//}
+
+
+
+
+
+
 mostraPergunta();
 
 function mostraAlternativas() {
